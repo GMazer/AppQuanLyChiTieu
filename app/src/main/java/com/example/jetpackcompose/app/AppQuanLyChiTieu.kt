a887d2ab033514d84cf875dedeba9c5975fdc1af
NCTona <mnbtnbnk1906@gmail.com>
2024-11-21 22:48:55 +0700

    handle_token

--- app/src/main/java/com/example/jetpackcompose/app/AppQuanLyChiTieu.kt
+++ app/src/main/java/com/example/jetpackcompose/app/AppQuanLyChiTieu.kt
@@ -1,27 +1,47 @@
 package com.example.jetpackcompose.app
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.jetpackcompose.app.screens.login_signup.SignUpScreen
 import com.example.jetpackcompose.app.screens.login_signup.SignInScreen
 import com.example.jetpackcompose.app.features.MainScreen
+import com.example.jetpackcompose.app.features.apiService.SignInViewModel
+import com.example.jetpackcompose.app.features.apiService.SignInViewModelFactory
 import com.example.jetpackcompose.app.screens.OtherScreen
 import com.example.jetpackcompose.app.screens.anual_sceens.AnualScreen
 
 @Composable
 fun AppQuanLyChiTieu() {
     val navController = rememberNavController()
+    val context = LocalContext.current
+    val signInViewModel: SignInViewModel = viewModel(
+        factory = SignInViewModelFactory(context) // Cung cấp Context cho ViewModel
+    )
 
-    NavHost(navController = navController, startDestination = "signup") {
-        composable("signup") { SignUpScreen(navController) }
-        composable("signin") { SignInScreen(navController) }
-        composable("mainscreen") { MainScreen(navController)}
-        composable("anual") { AnualScreen(navController) }
-        composable("other") { OtherScreen(navController)}
+    if (signInViewModel.isTokenCleared()) {
+        NavHost(navController = navController, startDestination = "signup") {
+            composable("signup") { SignUpScreen(navController) }
+            composable("signin") { SignInScreen(navController) }
+            composable("mainscreen") { MainScreen(navController)}
+            composable("anual") { AnualScreen(navController) }
+            composable("other") { OtherScreen(navController)}
+        }
+    } else {
+        NavHost(navController = navController, startDestination = "mainscreen") {
+            composable("signup") { SignUpScreen(navController) }
+            composable("signin") { SignInScreen(navController) }
+            composable("mainscreen") { MainScreen(navController)}
+            composable("anual") { AnualScreen(navController) }
+            composable("other") { OtherScreen(navController)}
+        }
     }
+
+
 }
 
 
