c17b55d3963646b07954a06b5992ab82f84bd92c
Hypear203 <ct060317@actvn.edu.vn>
2024-12-09 10:26:57 +0700

    Fix ondone search

---
+++ app/src/main/java/com/example/jetpackcompose/app/features/apiService/ReadNotificationTransaction/ReadTransactionNoti.kt
@@ -0,0 +1,29 @@
+package com.example.jetpackcompose.app.features.apiService.ReadNotificationTransaction
+
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
+
+class ReadTransactionNoti : NotificationListenerService() {
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        // Lấy nội dung thông báo
+        val notificationText = sbn.notification.extras.getString("android.text")
+
+        notificationText?.let {
+            // Kiểm tra xem thông báo có chứa thông tin về biến động số dư không
+            if (isBalanceChange(it)) {
+                Log.d("NotificationService", "Đã nhận thông báo biến động số dư: $it")
+                // Xử lý logic cho việc nhận diện số dư
+            }
+        }
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification) {
+        // Xử lý khi thông báo bị xóa (nếu cần)
+    }
+
+    private fun isBalanceChange(text: String): Boolean {
+        // Kiểm tra nếu thông báo chứa chuỗi số bắt đầu bằng dấu + hoặc -
+        return text.matches(".*[+-]\\d.*".toRegex())
+    }
+}
--- app/src/main/java/com/example/jetpackcompose/app/screens/find_calendar/FindCalendarScreen.kt
+++ app/src/main/java/com/example/jetpackcompose/app/screens/find_calendar/FindCalendarScreen.kt
@@ -1,77 +1,61 @@
 package com.example.jetpackcompose.app.screens.find_calendar
 
+import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Divider
+import androidx.compose.material.IconButton
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import com.example.jetpackcompose.app.features.apiService.TransactionAPI.GetTransactionViewModel
-import com.example.jetpackcompose.components.montserrat
-import com.example.jetpackcompose.ui.theme.topBarColor
-import android.util.Log
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Divider
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.jetpackcompose.ui.theme.topBarColor
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.IconButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.navigation.NavController
 import com.example.jetpackcompose.R
-import com.example.jetpackcompose.app.features.apiService.ReportAPI.GetReportViewModel
 import com.example.jetpackcompose.app.features.apiService.TransactionAPI.FindTransactionViewModel
 import com.example.jetpackcompose.app.network.FindTransactionResponse
 import com.example.jetpackcompose.app.network.TransactionResponse
-import com.example.jetpackcompose.components.CategoryIconWithName
 import com.example.jetpackcompose.components.DayIndex
-import com.example.jetpackcompose.components.DonutChartWithProgress
-import com.example.jetpackcompose.components.MessagePopup
-import com.example.jetpackcompose.components.MonthPickerButton
-import com.example.jetpackcompose.components.NoteTextField
-import com.example.jetpackcompose.components.ReportTable
 import com.example.jetpackcompose.components.montserrat
 import com.example.jetpackcompose.ui.theme.colorPrimary
-import com.example.jetpackcompose.ui.theme.lightGray
+import com.example.jetpackcompose.ui.theme.componentShapes
 import com.example.jetpackcompose.ui.theme.textColor
-import java.util.Calendar
+import com.example.jetpackcompose.ui.theme.topBarColor
 
 fun mapFindTransactionToTransactionResponse(
     findTransactions: List<FindTransactionResponse>
@@ -103,23 +87,9 @@ fun FindCalendarScreen(navController: NavController) {
     var errorMessage by remember { mutableStateOf("") }
 
     var textNote by remember { mutableStateOf(TextFieldValue()) }
+    val keyboardController = LocalSoftwareKeyboardController.current
 
-    val customTypography = Typography(
-        bodyLarge = TextStyle(fontFamily = com.example.jetpackcompose.app.features.inputFeatures.montserrat),
-        bodyMedium = TextStyle(fontFamily = com.example.jetpackcompose.app.features.inputFeatures.montserrat),
-        bodySmall = TextStyle(fontFamily = com.example.jetpackcompose.app.features.inputFeatures.montserrat),
-        titleLarge = TextStyle(fontFamily = com.example.jetpackcompose.app.features.inputFeatures.montserrat),
-        titleMedium = TextStyle(fontFamily = com.example.jetpackcompose.app.features.inputFeatures.montserrat),
-        titleSmall = TextStyle(fontFamily = com.example.jetpackcompose.app.features.inputFeatures.montserrat),
-        labelLarge = TextStyle(fontFamily = com.example.jetpackcompose.app.features.inputFeatures.montserrat),
-        labelMedium = TextStyle(fontFamily = com.example.jetpackcompose.app.features.inputFeatures.montserrat),
-        labelSmall = TextStyle(fontFamily = com.example.jetpackcompose.app.features.inputFeatures.montserrat),
-        headlineLarge = TextStyle(fontFamily = com.example.jetpackcompose.app.features.inputFeatures.montserrat),
-        headlineMedium = TextStyle(fontFamily = com.example.jetpackcompose.app.features.inputFeatures.montserrat),
-        headlineSmall = TextStyle(fontFamily = com.example.jetpackcompose.app.features.inputFeatures.montserrat)
-    )
-
-    MaterialTheme(typography = customTypography) {
+    MaterialTheme {
         Scaffold(
             topBar = {
                 Column(
@@ -131,6 +101,7 @@ fun FindCalendarScreen(navController: NavController) {
                         title = {
                             Box(
                                 modifier = Modifier
+                                    .height(50.dp)
                                     .fillMaxWidth(),
                             ) {
                                 IconButton(
@@ -165,7 +136,6 @@ fun FindCalendarScreen(navController: NavController) {
                                         .align(Alignment.Center)
                                         .offset(x = (-8).dp)
                                 )
-
                             }
                         },
                         colors = TopAppBarDefaults.smallTopAppBarColors(
@@ -228,18 +198,29 @@ fun FindCalendarScreen(navController: NavController) {
                                             contentDescription = "Tìm kiếm",
                                         )
                                         Spacer(modifier = Modifier.width(8.dp))
-                                        Text(text = "Tìm kiếm")
+                                        Text(
+                                            text = "Tìm kiếm",
+                                            fontFamily = montserrat,
+                                        )
                                     }
                                 },
-                                shape = RoundedCornerShape(8.dp),
+                                shape = componentShapes.medium,
                                 modifier = Modifier.fillMaxWidth(),
                                 colors = TextFieldDefaults.outlinedTextFieldColors(
-                                    focusedBorderColor = colorPrimary, // Màu viền khi đang focus
-                                    unfocusedBorderColor = Color.Gray, // Màu viền khi không focus
-                                    disabledBorderColor = Color.LightGray, // Màu viền khi disabled
-                                    errorBorderColor = Color.Red, // Màu viền khi có lỗi
-                                    focusedLabelColor = colorPrimary, // Thay đổi màu label khi focus
-                                    unfocusedLabelColor = Color.Gray // Thay đổi màu label khi không focus
+                                    focusedBorderColor = colorPrimary,
+                                    unfocusedBorderColor = Color(0xffB0B0B0),
+                                    disabledBorderColor = Color.LightGray,
+                                    errorBorderColor = Color.Red,
+                                    focusedLabelColor = colorPrimary,
+                                    unfocusedLabelColor = Color(0xffB0B0B0),
+                                ),
+                                keyboardOptions = KeyboardOptions.Default.copy(
+                                    imeAction = ImeAction.Done
+                                ),
+                                keyboardActions = KeyboardActions(
+                                    onDone = {
+                                        keyboardController?.hide()
+                                    }
                                 )
                             )
                         }
@@ -260,4 +241,11 @@ fun FindCalendarScreen(navController: NavController) {
             }
         }
     }
+}
+
+
+@Composable
+@Preview
+fun FindCalendarScreenPreview() {
+    FindCalendarScreen(navController = NavController(LocalContext.current))
 }
